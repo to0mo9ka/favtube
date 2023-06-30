c3870acd359a11059252f0b167632ca11af6799a
EC2 Default User <ec2-user@ip-172-31-28-54.ap-northeast-1.compute.internal>
2023-06-30 04:26:46 +0000

    Initial commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,12 @@
+class UsersController < ApplicationController
+  def index
+    @video = Video.new
+  end
+
+  def show
+    @video = Video.new
+  end
+
+  def edit
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+
+  devise_for :users
+  root to: 'homes#top'
+  get 'home/about', to: 'homes#about'
+  resources :users, only: [:index, :show, :edit, :update] # 追加
+  resources :videos, only: [:index, :show, :create, :edit, :update, :destroy]
+end
