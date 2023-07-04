086b1b16cc1efa4798da3869495199a48a8f703e
EC2 Default User <ec2-user@ip-172-31-28-54.ap-northeast-1.compute.internal>
2023-07-04 10:40:16 +0000

    add video favorite

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,7 +1,15 @@
 class FavoritesController < ApplicationController
   def create
+    video = Video.find(params[:video_id])
+    favorite = current_user.favorites.new(video_id: video.id)
+    favorite.save
+    redirect_to video_path(video)
   end
 
   def destroy
+    video = Video.find(params[:video_id])
+    favorite = current_user.favorites.find_by(video_id: video.id)
+    favorite.destroy
+    redirect_to video_path(video)
   end
 end
--- app/models/post_comment.rb
+++ app/models/post_comment.rb
@@ -1,4 +1,5 @@
 class PostComment < ApplicationRecord
   belongs_to :user
   belongs_to :video
+  
 end
