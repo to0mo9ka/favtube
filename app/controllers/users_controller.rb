ee5354fd0ed74c705e5c971ee88962fff3961770
EC2 Default User <ec2-user@ip-172-31-28-54.ap-northeast-1.compute.internal>
2023-07-14 03:36:44 +0000

    Completed the framework of  Relationship

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,12 +14,12 @@ class UsersController < ApplicationController
   
   def follows
     user = User.find(params[:id])
-    @users = user.following_user.page(params[:page]).per(3).reverse_order
+    @users = user.following_user.page(params[:page]).per(10).reverse_order
   end
 
   def followers
     user = User.find(params[:id])
-    @users = user.follower_user.page(params[:page]).per(3).reverse_order
+    @users = user.follower_user.page(params[:page]).per(10).reverse_order
   end
   
   def edit
