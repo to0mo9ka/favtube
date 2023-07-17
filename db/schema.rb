bb655be2f2d87d51052be98559e30cc70d29f8bb
EC2 Default User <ec2-user@ip-172-31-28-54.ap-northeast-1.compute.internal>
2023-07-17 00:09:01 +0000

    Add account_type column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_13_233104) do
+ActiveRecord::Schema.define(version: 2023_07_16_235526) do
 
   create_table "favorite_comments", force: :cascade do |t|
     t.integer "user_id"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 2023_07_13_233104) do
     t.string "profile_image_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "account_type", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
