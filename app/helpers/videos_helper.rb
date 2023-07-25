349509afb35d149ead2b1f904d61adac446f13aa
EC2 Default User <ec2-user@ip-172-31-28-54.ap-northeast-1.compute.internal>
2023-07-25 01:23:02 +0000

    &enablejsapi=1をつけた

--- app/helpers/videos_helper.rb
+++ app/helpers/videos_helper.rb
@@ -2,7 +2,7 @@ module VideosHelper
   def find_youtube_url(youtube_url)
     if youtube_url.match?(%r{^(https?://)?(www\.)?(youtu\.be/|youtube\.com/watch\?v=)([\w-]+)(\S+)?$})
       extracted_url = youtube_url.strip.gsub(%r{^(https?://)?(www\.)?(youtu\.be/|youtube\.com/watch\?v=)}, "")
-      extracted_url.gsub!(/\?t=(\d+)s?/, "?start=\\1") # t=数字部分を?start=の後に置換
+      extracted_url.gsub!(/\?t=(\d+)s?/, "?start=\\1&enablejsapi=1") # t=数字部分を?start=の後に置換
       extracted_url
     end
   end
