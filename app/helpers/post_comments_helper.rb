ef67657c541a4b033b46e8a63b73454e5f079e27
EC2 Default User <ec2-user@ip-172-31-28-54.ap-northeast-1.compute.internal>
2023-07-30 04:11:03 +0000

    一旦動くうちに保存

--- app/helpers/post_comments_helper.rb
+++ app/helpers/post_comments_helper.rb
@@ -1,2 +1,13 @@
 module PostCommentsHelper
+  def find_youtube_url(youtube_url)
+    if youtube_url.match?(%r{^(https?://)?(www\.)?(youtu\.be/|youtube\.com/watch\?v=)([\w-]+)(\S+)?$})
+      extracted_url = youtube_url.strip.gsub(%r{^(https?://)?(www\.)?(youtu\.be/|youtube\.com/watch\?v=)}, "")
+      if youtube_url.include?("?")
+        extracted_url.gsub!(/\?t=(\d+)s?/, "?start=\\1&enablejsapi=1") # t=数字部分を?start=の後に置換
+      else
+        extracted_url += "?enablejsapi=1"
+      end
+      extracted_url
+    end
+  end
 end
--- app/helpers/videos_helper.rb
+++ app/helpers/videos_helper.rb
@@ -2,7 +2,11 @@ module VideosHelper
   def find_youtube_url(youtube_url)
     if youtube_url.match?(%r{^(https?://)?(www\.)?(youtu\.be/|youtube\.com/watch\?v=)([\w-]+)(\S+)?$})
       extracted_url = youtube_url.strip.gsub(%r{^(https?://)?(www\.)?(youtu\.be/|youtube\.com/watch\?v=)}, "")
-      extracted_url.gsub!(/\?t=(\d+)s?/, "?start=\\1&enablejsapi=1") # t=数字部分を?start=の後に置換
+      if youtube_url.include?("?")
+        extracted_url.gsub!(/\?t=(\d+)s?/, "?start=\\1&enablejsapi=1") # t=数字部分を?start=の後に置換
+      else
+        extracted_url += "?enablejsapi=1"
+      end
       extracted_url
     end
   end
