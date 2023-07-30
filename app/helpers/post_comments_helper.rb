7dda0dffeddb789137745730f479de44fa03f355
EC2 Default User <ec2-user@ip-172-31-28-54.ap-northeast-1.compute.internal>
2023-07-30 07:42:10 +0000

    コメント欄再生時間指定リンクつけた

--- app/helpers/post_comments_helper.rb
+++ app/helpers/post_comments_helper.rb
@@ -1,13 +1,26 @@
 module PostCommentsHelper
-  def find_youtube_url(youtube_url)
-    if youtube_url.match?(%r{^(https?://)?(www\.)?(youtu\.be/|youtube\.com/watch\?v=)([\w-]+)(\S+)?$})
-      extracted_url = youtube_url.strip.gsub(%r{^(https?://)?(www\.)?(youtu\.be/|youtube\.com/watch\?v=)}, "")
-      if youtube_url.include?("?")
-        extracted_url.gsub!(/\?t=(\d+)s?/, "?start=\\1&enablejsapi=1") # t=数字部分を?start=の後に置換
-      else
-        extracted_url += "?enablejsapi=1"
-      end
-      extracted_url
+  def linkify_comment(comment, video_id)
+    escaped_comment = h(comment) # HTMLエスケープを行う
+
+    # 再生時間の部分だけをエスケープ解除してリンクに変換
+    escaped_comment.gsub!(time_regex) do |time|
+      # 時間、分、秒を取得する
+      time_parts = time.split(':').map(&:to_i)
+      hours = time_parts.size == 3 ? time_parts[0] : 0
+      minutes = time_parts[-2]
+      seconds = time_parts[-1]
+
+      time_in_seconds = hours * 3600 + minutes * 60 + seconds
+      "<a href onclick=\"clickTime(#{time_in_seconds}); return false\">#{time}</a>"
     end
+
+    # エスケープ解除されたコメントをHTMLエスケープして安全に表示
+    escaped_comment.html_safe
+  end
+
+  private
+
+  def time_regex
+    /\b(?:\d{1,2}:)?\d{1,2}:\d{2}\b/
   end
 end
